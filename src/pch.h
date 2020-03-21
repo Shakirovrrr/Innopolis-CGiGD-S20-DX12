0ad43af32e16ca4f35d3af2b64bab6e165c06d04
Ruslan Shakirov <shakirovrrr@outlook.com>
2020-03-21 15:45:30 +0300

    Add precompiled header

---
+++ src/pch.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef PCH_H
+#define PCH_H
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <Windows.h>
+
+#include <wrl.h>
+
+#include <initguid.h>
+
+#include <dxgi1_4.h>
+#include "d3dx12.h"
+
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+
+#include <iostream>
+
+#include <exception>
+
+#endif // !PCH_H
