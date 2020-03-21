0c1b45e6663e1332432dc9adb28e61fbba40a9d0
Ruslan Shakirov <shakirovrrr@outlook.com>
2020-03-21 15:47:55 +0300

    Format shaders

--- shaders/shaders.hlsl
+++ shaders/shaders.hlsl
@@ -1,11 +1,13 @@
-struct PSInput
-{
+cbuffer ConstantBuffer: register(b0) {
+	float4x4 mwpMatrix;
+}
+
+struct PSInput {
 	float4 position : SV_POSITION;
 	float4 color : COLOR;
 };
 
-PSInput VSMain(float4 position : POSITION, float4 color : COLOR)
-{
+PSInput VSMain(float4 position : POSITION, float4 color : COLOR) {
 	PSInput result;
 
 	result.position = position;
@@ -14,7 +16,6 @@ PSInput VSMain(float4 position : POSITION, float4 color : COLOR)
 	return result;
 }
 
-float4 PSMain(PSInput input) : SV_TARGET
-{
+float4 PSMain(PSInput input) : SV_TARGET {
 	return input.color;
 }
